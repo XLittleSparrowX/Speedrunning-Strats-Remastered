ca5eeef5562d4dad60f4dd6c643c101d40e5293e
Evie Sparrow <41015392+XLittleSparrowX@users.noreply.github.com>
2019-06-04 18:03:58 +0100

    Added a few settings.

---
+++ Resources/l3m5sd.lua
@@ -0,0 +1,4 @@
+if GetSetting("L3M5Spawn") then
+    Redirect (GetModPath() .. "/Resources/Spawns/Level03/l3m5sdSpawn.mfk")
+end
+
---
+++ Resources/l5m3sd.lua
@@ -0,0 +1,3 @@
+if GetSetting("L5M3Spawn") then
+    Redirect (GetModPath() .. "/Resources/Spawns/Level05/m3sdSpawn.mfk")
+end
