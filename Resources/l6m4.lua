28f7b70220ec455f20e5ddc8506cbf1789b5c5ee
Evie Sparrow <41015392+XLittleSparrowX@users.noreply.github.com>
2019-06-04 14:49:05 +0100

    Added Duff For Me Practice Options

---
+++ Resources/l6m4.lua
@@ -0,0 +1,20 @@
+local car
+local  tick
+
+local car_opt = GetSetting("L6M4Car")
+if car_opt == 0 then
+   car = "CBFH"
+elseif car_opt == 1 then
+   car = "Globex"
+else
+   car = "OWRC"
+end
+
+local tick_opt = GetSetting("L6M4Spawn")
+if tick_opt == 0 then
+   tick = "Spawn"
+else
+   tick = "Vanilla"
+end
+
+Redirect(GetModPath() .. "/Resources/Spawns/level06/m6" .. tick .. car .. ".mfk")
